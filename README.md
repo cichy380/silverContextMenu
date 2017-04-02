1c5ceb30faad4185757b9749887ee2d175207173
Marcin Dobroszek <marcin.dobroszek@gmail.com>
2017-04-02 12:23:42 +0200

    initial commit

---
+++ README.md
@@ -0,0 +1,38 @@
+# Silver Context Menu
+
+It is very simply [jQuery](https://jquery.com/) plugin based on example from ["jQuery in Action, Third Edition" book](http://manning.com/books/jquery-in-action-third-edition).
+
+* Demo: [http://templates.silversite.pl/silver-context-menu/](http://templates.silversite.pl/silver-context-menu/)
+
+## Quick start
+
+Put the script at the [bottom](https://developer.yahoo.com/performance/rules.html#js_bottom) of your markup right after jQuery:
+
+```html
+<script src="jquery.js"></script>
+<script src="jquery.silver.contextMenu.js"></script>
+```
+
+### Usage
+
+Create your context menu HTML and put it at any place on your HTML code. Do not forget set hidden this element by CSS. Example:
+
+```html
+<ul id="contextmenu" style="display: none; ">
+    <li><a href="#1">First option</a></li>
+    <li><a href="#2">Second option</a></li>
+    <li><a href="#3">Third option</a></li>
+</ul>
+```
+
+Call the [plugin](https://learn.jquery.com/plugins/) function on the area element which you want to using context menu with one required option - selector of your menu. And it is ready.
+
+```javascript
+$('.element-needs-contextmenu').silverContextMenu({
+    menuSelector: '#contextmenu'
+});
+```
+
+## License
+
+Code released under the MIT license.
---
+++ src/jquery.silver.contextMenu.js
@@ -0,0 +1,105 @@
+/**
+ * Silver Context Menu
+ *
+ * Context Menu plugin based on example plugin from "jQuery in Action, Third Edition" book
+ *
+ * @version 0.0.1
+ * @author Marcin Dobroszek
+ * @license The MIT License (MIT)
+ */
+;(function($, undefined) {
+    var namespace = 'silverContextMenu';
+    //var defaultOptions = {};
+
+    /**
+     * All available methods.
+     * @private
+     */
+    var methods = {
+
+        /**
+         * Creates a ContextMenu.
+         * @param {Object} [config] - The options
+         */
+        init: function(config) {
+            var options = $.extend({}, $.fn.silverContextMenu.defaults, config),
+                $menu = $(options.menuSelector);
+
+            // check if required options are configured
+            if (!options.menuSelector) {
+                $.error('Required option "menuSelector" was not configured.')
+            }
+
+            // check if menu was found
+            if ($menu.length === 0) {
+                $.error('Menu element was not found.')
+            }
+
+            // check if plugin was not initialized before on this element
+            if (this.filter(function() {return $(this).data(namespace);}).length !== 0) {
+                $.error('Test-01 is already initialized.')
+            }
+            this.data(namespace, options);
+
+            // mouse, right click (contextmenu) event - open ContextMenu
+            this.on('contextmenu.' + namespace, function(event) {
+                event.preventDefault();
+                $menu
+                    .css({
+                        position: 'absolute',
+                        top: event.pageY,
+                        left: event.pageX,
+                    })
+                    .show();
+            });
+
+            // mouse click event on any webiste element except the ContextMenu - close ContextMenu
+            $('html').on('click.' + namespace, function(event) {
+                if ($(event.target).closest($menu).length === 0) { // clicked outside $menu
+                    $menu.hide();
+                }
+            });
+
+            return this; // jQuery Method Chaining
+        },
+
+        /**
+         * Destroys a ContextMenu.
+         */
+        destroy: function() {
+            this
+                .each(function() {
+                    var options = $(this).data(namespace);
+                    $(options.menuSelector).hide();
+                })
+                .removeData(namespace)
+                .add('html').off('.' + namespace); // destroy all plugin events
+
+            return this; // jQuery Method Chaining
+        }
+    };
+
+    // definition of plugin
+    $.fn.silverContextMenu = function(method) {
+        if ($.type(method) === 'string' && methods[method]) {
+            methods[method].apply(
+                this,
+                Array.prototype.slice.call(arguments, 1)
+            );
+        }
+        else if ($.type(method) === 'object') { // got object with configuration - init() method start
+            methods.init.apply(this, arguments);
+        }
+        else if (method === undefined) { // got nothing
+            $.error('Method name or/and configuration object is required.')
+        }
+        else {
+            $.error('Method ' + method + ' does not exists.')
+        }
+    };
+
+    // make defaults configuration available outside
+    $.fn.silverContextMenu.defaults = {
+        menuSelector: null
+    };
+})(jQuery);
